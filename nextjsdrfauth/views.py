4e4a84585017c279eb4c66fca75d685be890da23
deshondixon <deshondixon@gmail.com>
2023-02-19 19:06:42 -0800

    acp

--- nextjsdrfauth/views.py
+++ nextjsdrfauth/views.py
@@ -1,11 +1,11 @@
-from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
-from dj_rest_auth.registration.views import SocialLoginView
-from allauth.socialaccount.providers.oauth2.client import OAuth2Client
-from django.conf import settings
+# from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+# from dj_rest_auth.registration.views import SocialLoginView
+# from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+# from django.conf import settings
 
 
-class GoogleLoginView(SocialLoginView):
-    authentication_classes = []
-    adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:3000"
-    client_class = OAuth2Client
+# class GoogleLoginView(SocialLoginView):
+#     authentication_classes = []
+#     adapter_class = GoogleOAuth2Adapter
+#     callback_url = "http://localhost:3000"
+#     client_class = OAuth2Client
