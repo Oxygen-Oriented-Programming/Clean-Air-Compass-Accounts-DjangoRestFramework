e972485ad5026d1d9379c56c82e065e83e5d05a0
Oliver Speir <oliverspeir9@gmail.com>
2023-02-20 20:03:03 -0700

    remove

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,29 +1,9 @@
 from django.conf import settings
 from rest_framework import serializers
-from library.socaillib import github
 from library.socaillib import google
 from library.register.register import register_social_user
 from rest_framework.exceptions import *
 
-
-class GithubSocialAuthSerializer(serializers.Serializer):
-    """Handles serialization of facebook related data"""
-
-    auth_token = serializers.CharField()
-
-    def validate_auth_token(self, auth_token):
-        user_data = github.Github.validate(auth_token)
-
-        try:
-            email = user_data["email"]
-            provider = "github"
-        except:
-            raise serializers.ValidationError(
-                "The token  is invalid or expired. Please login again."
-            )
-        return register_social_user(
-            provider=provider, user_id=None, email=email, name=None
-        )
 
 class GoogleSocialAuthSerializer(serializers.Serializer):
     auth_token = serializers.CharField()
