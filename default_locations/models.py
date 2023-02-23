be4b59df72759bffa163aad7198b3cbd4d103675
Jason Christopher <jchristopher2448@gmail.com>
2023-02-22 23:23:39 -0600

    updated user model, make migrations after pulling

--- default_locations/models.py
+++ default_locations/models.py
@@ -3,5 +3,5 @@ from accounts.models import User
 
 
 class DefaultLocation(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_location = models.CharField(max_length=100, default='Chicago', primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    default_location = models.CharField(max_length=100, unique=True)
