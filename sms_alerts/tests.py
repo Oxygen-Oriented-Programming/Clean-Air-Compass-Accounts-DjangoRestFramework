c6d36c8d5e99a61f5d465bc1dfa6aec214df6b42
Harper Foley <harper.e.foley@gmail.com>
2023-02-24 10:58:09 -0800

    Adds functioning tests to each module

--- sms_alerts/tests.py
+++ sms_alerts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from sms_alerts.models import SmsAlert
+from accounts.models import User
 
-# Create your tests here.
+
+class SmsAlertTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass', email='testuser@example.com')
+        self.sms_alert = SmsAlert.objects.create(
+            user=self.user,
+            phone_number='8302795914',
+            location='Seattle',
+            air_quality_threshold='Good'
+        )
+
+    def test_sms_alert_created(self):
+        self.assertEqual(self.sms_alert.user, self.user)
+        self.assertEqual(self.sms_alert.phone_number, '8302795914')
+        self.assertEqual(self.sms_alert.location, 'Seattle')
+        self.assertEqual(self.sms_alert.air_quality_threshold, 'Good')
+        self.assertEqual(self.sms_alert.previous_air_quality_threshold_alert, '')
+
+    def test_sms_alert_string_representation(self):
+        expected_string = f'{self.user.username} - Seattle - AQI Good'
+        self.assertEqual(str(self.sms_alert), expected_string)
